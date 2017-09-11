e14d7bb2fe84c28081bd7b1dbf2b6cf705f53410
twincharged <joseph.cs.ritchie@gmail.com>
2017-09-10 21:40:38 -0700

    [issues/1] restore cname

--- site/scripts/index.js
+++ site/scripts/index.js
@@ -1,5 +1,5 @@
 async function interpolateCodeExamples() {
-    let script = await $.getScript('examples/schemas/User.js')
+    let script = await $.getScript('examples/models/User.js')
     document.querySelector('code.example1').innerHTML = script.replace(/(?:\r\n|\r|\n)/g, '<br />')
 }
 
