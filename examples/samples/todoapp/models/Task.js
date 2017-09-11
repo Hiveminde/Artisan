40f3fc19220882dff80f3113e0fe4e3d54af36f2
Joseph Kwon <kwonjosepho@gmail.com>
2017-09-10 21:59:40 -0700

    [issues/5] Configure browser to work

--- examples/samples/todoapp/models/Task.js
+++ examples/samples/todoapp/models/Task.js
@@ -1,33 +1,35 @@
 try {
-  const Artisan = require('Artisan');
-} catch (e) {
-  console.log(Artisan)
-  const Artisan = Artisan || {
-    call: () => {}
-  }
+    Artisan = require('../../src/lib/Artisan')
+} catch (error) {
+    console.error(error.toString())
 }
 
+class Task extends Artisan {
 
-window.Task = function(attrs) {
-  Artisan.call({
-    adapters: {
-      client: 'HTTP',
-      server: 'Mongo'
-    },
-    schema: {
-      id: Artisan.types.ID,
-      title: Artisan.types.String,
-      complete: Artisan.types.Boolean
+    constructor(attrs) {
+        super({
+            adapters: {
+                client: 'NOOP',
+                server: 'NOOP'
+            },
+            schema: {
+                title: Artisan.types.String,
+                complete: Artisan.types.String
+            }
+        })
+
+        Object.assign(this, attrs)
     }
-  });
-  Object.assign(this, attrs)
+
+    beforeCreate() {
+
+    }
+
 }
-// Task.prototype = Object.create(Artisan.prototype)
-// Task.prototype.constructor = Task
-window.Task.prototype.read = function() {console.log('its getting')}
-window.Task.prototype.create = function() {console.log('created something')}
-try {
-  module.exports = window.Task
-} catch (e) {
-  window.Task = new Task()
+
+
+if (Artisan.environment() === 'SERVER') {
+    module.exports = Task
+} else {
+    window.Task = Task
 }
