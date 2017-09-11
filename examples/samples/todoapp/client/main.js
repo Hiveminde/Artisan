de33a3d318c19a054867fe5930984ee7bf1cad83
Joseph Kwon <kwonjosepho@gmail.com>
2017-09-10 20:21:06 -0700

    [issues/5] Some tiny progress on those requires

--- examples/samples/todoapp/client/main.js
+++ examples/samples/todoapp/client/main.js
@@ -1,41 +1,35 @@
-(async() => {
-  var Task = await requirejs(['../models/Task'])
-  Task = new Task();
-  console.log(Task);
-  let tasks = []
+let tasks = []
 
-
-  window.toggleComplete = async(event) => {
-    let li = event.target
-    let task = tasks.filter((task) => task.id === li.taskId)[0]
-    try {
-      await task.update({
-        complete: !task.complete
-      })
-      li.style.textDecoration = task.complete ? "line-through" : "none"
-    } catch (e) {
-      // e == 'Error: failed to update task with title: have fun'
-      $('#errorMsg').innerHTML = e.toString()
-      setTimeout(() => {
-        $('#errorMsg').innerHTML = ''
-      }, 10000)
-    }
-  }
-
-  window.submit = async(event) => {
-    let task = await Task.create({
-      title: document.getElementById('title').value
+window.toggleComplete = async(event) => {
+  let li = event.target
+  let task = tasks.filter((task) => task.id === li.taskId)[0]
+  try {
+    await task.update({
+      complete: !task.complete
     })
-    tasks.push(task)
+    li.style.textDecoration = task.complete ? "line-through" : "none"
+  } catch (e) {
+    // e == 'Error: failed to update task with title: have fun'
+    $('#errorMsg').innerHTML = e.toString()
+    setTimeout(() => {
+      $('#errorMsg').innerHTML = ''
+    }, 10000)
   }
+}
 
-  window.refreshTasks = async() => {
-    tasks = await Task.read()
-    $('#taskList').innerHTML = ''
-    tasks.forEach((task) => {
-      $('#taskList').innerHTML += `<li task-id="${task.id}" on-click="window.toggleComplete(this)">${task.title}</li>`
-    })
-  }
+window.submit = async(event) => {
+  let task = await Task.create({
+    title: document.getElementById('title').value
+  })
+  tasks.push(task)
+}
 
-  setTimeout(window.refreshTasks, 15000)
-})()
+window.refreshTasks = async() => {
+  tasks = await Task.read()
+  $('#taskList').innerHTML = ''
+  tasks.forEach((task) => {
+    $('#taskList').innerHTML += `<li task-id="${task.id}" on-click="window.toggleComplete(this)">${task.title}</li>`
+  })
+}
+
+setTimeout(window.refreshTasks, 15000)
--- examples/samples/todoapp/models/Task.js
+++ examples/samples/todoapp/models/Task.js
@@ -1,24 +1,33 @@
-// const Artisan = require('Artisan');
+try {
+  const Artisan = require('Artisan');
+} catch (e) {
+  console.log(Artisan)
+  const Artisan = Artisan || {
+    call: () => {}
+  }
+}
 
-const Task = function(attrs) {
-  // Artisan.call({
-  //   adapters: {
-  //     client: 'HTTP',
-  //     server: 'Mongo'
-  //   },
-  //   schema: {
-  //     id: Artisan.types.ID,
-  //     title: Artisan.types.String,
-  //     complete: Artisan.types.Boolean
-  //   }
-  // });
+
+window.Task = function(attrs) {
+  Artisan.call({
+    adapters: {
+      client: 'HTTP',
+      server: 'Mongo'
+    },
+    schema: {
+      id: Artisan.types.ID,
+      title: Artisan.types.String,
+      complete: Artisan.types.Boolean
+    }
+  });
   Object.assign(this, attrs)
 }
 // Task.prototype = Object.create(Artisan.prototype)
-Task.prototype.constructor = Task
-Task.prototype.read = function() {console.log('its getting')}
-Task.prototype.create = function() {console.log('created something')}
+// Task.prototype.constructor = Task
+window.Task.prototype.read = function() {console.log('its getting')}
+window.Task.prototype.create = function() {console.log('created something')}
 try {
-  module.exports = Task
+  module.exports = window.Task
 } catch (e) {
+  window.Task = new Task()
 }
